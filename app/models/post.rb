78e7185889e119408282905927ccf6f062e89b32
Tim Booker <booker.tim@gmail.com>
2013-06-24 01:33:03 +0100

    First go

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ActiveRecord::Base
+  attr_accessible :content, :name, :title
+  has_many :comments
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyEventmob::Application.config.secret_token = '499fd0c03962db8fdce2b5dfce9221aede94399997db6bf5af72371d9e6e8ef27ea6280fe599ef6a34363708c744097bd5032129a0d415b5083babf5cae814d5'
